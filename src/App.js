5f9dcfad88176d256a7f00641e1938b9bc37d5bc
Anhanh1412 <nguyenanh141297@gmail.com>
2022-03-28 23:49:45 +0700

    update28-3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,55 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>todo list reactJS</h1>
+      <h1 className="todo">todo list reactJS</h1>
+      <div className='table'>
+      <table className='tb1'>
+        <tr className='title-1'>
+          <p className='conten1'> Add a tank ???</p>
+        </tr>
+        <tr>
+          <p className='conten2'> Item</p>
+        </tr>
+        <tr>
+          <p>
+            <input className='conten3' placeholder='What do you want todo'></input>
+          </p>
+        </tr>
+        <tr>
+          <p className='conten4'>Enter what you want to</p>
+        </tr>
+        <tr>
+          <p>
+            <button className='btn'>Submit</button>
+          </p>
+        </tr>
+      </table>
+
+      <table>
+        <tr className='title-1'>
+          <p className='conten1'>Task</p>
+        </tr>
+        <tr className='row2'>
+          <th className='col1'><p>Item </p></th>
+          <th className='col2'><p>Statuts </p></th>
+          <th className='col3'><p>Action </p></th>
+        </tr>
+        <tr className='row2'>
+          <th className='col1 f-w'><p>Buy Benz </p></th>
+          <th className='col2 f-w'><p>Not Complete </p></th>
+          <th className='col3'><p>
+            <button className='btn'>Complete</button>
+            <button className='btn mgl bgcl'>Delete</button>
+          </p>
+
+          </th>
+        </tr>
+      </table>
+
+      </div>
     </div>
   );
 }
