4b4ff900d6c4715609c61cae1e72c63d8ab93845
Anhanh1412 <nguyenanh141297@gmail.com>
2022-03-29 15:14:02 +0700

    update29-3

--- src/App.js
+++ src/App.js
@@ -5,52 +5,37 @@ function App() {
   return (
     <div className="App">
       <h1 className="todo">todo list reactJS</h1>
-      <div className='table'>
-      <table className='tb1'>
-        <tr className='title-1'>
-          <p className='conten1'> Add a tank ???</p>
-        </tr>
-        <tr>
-          <p className='conten2'> Item</p>
-        </tr>
-        <tr>
-          <p>
-            <input className='conten3' placeholder='What do you want todo'></input>
-          </p>
-        </tr>
-        <tr>
-          <p className='conten4'>Enter what you want to</p>
-        </tr>
-        <tr>
-          <p>
-            <button className='btn'>Submit</button>
-          </p>
-        </tr>
-      </table>
+      <div className='head'>
+        <div className='title-1'>Add a task</div>
+        <div className='name-item'>Item</div>
+        <div><input className='input' placeholder='What do you want to do ???'></input></div>
+        <div className='note'>Enter what you want</div>
+        <div><button className='btn submit'>Submit</button></div>
+      </div>
+        
 
-      <table>
-        <tr className='title-1'>
-          <p className='conten1'>Task</p>
-        </tr>
-        <tr className='row2'>
-          <th className='col1'><p>Item </p></th>
-          <th className='col2'><p>Statuts </p></th>
-          <th className='col3'><p>Action </p></th>
-        </tr>
-        <tr className='row2'>
-          <th className='col1 f-w'><p>Buy Benz </p></th>
-          <th className='col2 f-w'><p>Not Complete </p></th>
-          <th className='col3'><p>
-            <button className='btn'>Complete</button>
-            <button className='btn mgl bgcl'>Delete</button>
-          </p>
+        <table>
+          <tr className='title-1'>
+            Task
+          </tr>
+          <tr className='row2'>
+            <th className='col1'>Item</th>
+            <th className='col2'>Statuts </th>
+            <th className='col3'>Action </th>
+          </tr>
+          <tr className='row2'>
+            <th className='col1 f-w'>Buy Benz </th>
+            <th className='col2 f-w'>Not Complete </th>
+            <th className='col3'>
+              <button className='btn'>Complete</button>
+              <button className='btn mgl bgcl'>Delete</button>           
 
-          </th>
-        </tr>
-      </table>
+            </th>
+          </tr>
+        </table>
 
       </div>
-    </div>
+    
   );
 }
 
